e4b3cc197e4cee24c92ba73ceb3a59fdccd4147e
yangnian <15399230309@163.com>
2020-01-03 00:30:19 +0800

    信息加密

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ def init_mail(app):
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USE_SSL'] = False
     app.config['MAIL_USERNAME'] = '327248460@qq.com'
-    app.config['MAIL_PASSWORD'] = 'lrdzymravyihbgch'
+    app.config['MAIL_PASSWORD'] = 'xxxxx'
     app.config['MAIL_DEFAULT_SENDER'] = 'kissyangnian@qq.com'
     app.MAIL_DEFAULT_SENDER = 'kissyangnian@qq.com'
     mail = Mail(app)
